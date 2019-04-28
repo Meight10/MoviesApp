598a9c8062684cbc7b924ae03d1f1fb0d51e701a
Antonio Pertuz <antonio.pertuz@grupodisatel.com>
2019-04-28 16:17:42 -0600

    including loader while suggestionList loads

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Text} from 'react-native';
+import {Text, View, ActivityIndicator} from 'react-native';
 
 import Home from './src/screens/containers/Home';
 import Header from './src/sections/components/Header';
@@ -11,25 +11,33 @@ export default class App extends Component<Props> {
 
     state = {
         suggestionList: [],
+        loading: true,
     };
 
     async componentDidMount(): void {
         const movies = await API.getSuggestion(10);
         console.log(movies);
         this.setState({
-            suggestionList: movies
+            suggestionList: movies,
+            loading: false,
         })
     }
 
     render() {
+        const {suggestionList, loading} = this.state;
         return (
             <Home>
                 <Header />
                 <Text>buscador</Text>
                 <Text>categorias</Text>
+                {loading ?
+                <View>
+                    <ActivityIndicator size="large" color = "#ff050"/>
+                </View>
+                :
                 <SuggestionList
-                    list = {this.state.suggestionList}
-                />
+                    list = {suggestionList}
+                />}
             </Home>
         );
     }
